51dfb9d4786d52ac9c47a3b7972bf0abc5859b05
a-nakatani <a-nakatani@KFD106.kf-c.co.jp>
2025-09-18 17:34:07 +0900

    fix:Task25　MapperとServiceクラス削除

--- trial_source/src/main/java/jp/co/sss/lms/mapper/Attendance30Mapper.java
+++
@@ -1,5 +0,0 @@
-package jp.co.sss.lms.mapper;
-
-public class AttendanceMapper {
-
-}
--- trial_source/src/main/java/jp/co/sss/lms/service/Attendance30Service.java
+++
@@ -1,9 +0,0 @@
-package jp.co.sss.lms.service;
-
-import org.springframework.stereotype.Service;
-//↓09/12 過去日未入力確認ダイアログ
-@Service
-public class AttendanceService {
-
-
-}
